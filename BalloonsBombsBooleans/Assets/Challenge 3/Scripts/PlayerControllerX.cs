331b1d2b91dfe46e82a9288c7991ee313e9fad6a
Tara Mallick <taramallick09@gmail.com>
2023-12-13 13:31:22 -0500

    bonus 1 complete

--- BalloonsBombsBooleans/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ BalloonsBombsBooleans/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -38,6 +38,11 @@ public class PlayerControllerX : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
+        if (transform.position.y > 15)
+        {
+            transform.position = new Vector3(transform.position.x, 15, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
